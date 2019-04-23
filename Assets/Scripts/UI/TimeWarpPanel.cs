1c27a7f1063406b43ed4265bc6fafb84e818dfb6
andatr <malcev.alexander@gmail.com>
2019-04-23 23:52:13 +0100

    time warp ui

---
+++ Assets/Scripts/UI/TimeWarpPanel.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Osim.UI
+{
+    public class TimeWarpPanel : MonoBehaviour
+    {
+        public int[] items;
+        public GameObject itemPrefab;
+        public GameObject hoverText;
+        public Color activeColor;
+        public Color inactiveColor;
+
+        private TimeWarpPanelItem[] _panelItems;
+        private Text _textObject;
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void Start()
+        {
+            if (itemPrefab == null || items == null || items.Length == 0)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+            _panelItems = new TimeWarpPanelItem[items.Length];
+            for (int i = 0; i < items.Length; ++i)
+                _panelItems[i] = NewItem(i);
+            SetWidth();
+            OnHoverExit(0);
+            OnItemClick(0);
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        private TimeWarpPanelItem NewItem(int index)
+        {
+            var itemObject = Instantiate(itemPrefab);
+            itemObject.name = itemPrefab.name + items[index];
+            itemObject.SetParent(gameObject);
+
+            var panelItem = itemObject.GetComponent<TimeWarpPanelItem>();
+            panelItem.Init(activeColor, inactiveColor);
+            panelItem.RegisterCallback(index, OnItemClick, OnHoverEnter, OnHoverExit);
+            return panelItem;
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        private void SetWidth()
+        {
+            var rect = GetComponent<RectTransform>();
+            var childRect = itemPrefab.GetComponent<RectTransform>();
+            var layout = GetComponent<HorizontalLayoutGroup>();
+            var width = layout.padding.right + layout.padding.left +
+                        layout.spacing * (items.Length - 1) + items.Length * childRect.sizeDelta.x;
+            rect.sizeDelta = new Vector2(width, rect.sizeDelta.y);
+            SetHoverTextPosition(width);
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        private void SetHoverTextPosition(float left)
+        {
+            if (hoverText == null) return;
+            _textObject = hoverText.GetComponent<Text>();
+            var rect = hoverText.GetComponent<RectTransform>();
+            rect.anchoredPosition = new Vector2(5.0f + left, 0.0f);
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        private void OnItemClick(int index)
+        {
+            for (int i = 0; i <= index; ++i)
+                _panelItems[i].active = true;
+            for (int i = index + 1; i < _panelItems.Length; ++i)
+                _panelItems[i].active = false;
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        private void OnHoverEnter(int index)
+        {
+            if (hoverText == null || _textObject == null) return;
+            hoverText.SetActive(true);
+            _textObject.text = "x" + items[index];
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        private void OnHoverExit(int index)
+        {
+            if (hoverText == null) return;
+                hoverText.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TimeWarpPanelItem.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Osim.UI
+{
+    public class TimeWarpPanelItem : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        private int _index;
+        private System.Action<int> _clickCallback;
+        private System.Action<int> _hoverEnterCallback;
+        private System.Action<int> _hoverExitCallback;
+        private Image _image;
+        public Color _activeColor;
+        public Color _inactiveColor;
+        private bool _active;
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public bool active
+        {
+            get { return _active; }
+            set
+            {
+                _active = value;
+                if (_image != null)
+                    _image.color = _active ? _activeColor : _inactiveColor;
+            }
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void Start()
+        {
+            _image = GetComponent<Image>();
+            _image.color = _active ? _activeColor : _inactiveColor;
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void OnDestroy()
+        {
+            _clickCallback = null;
+            _hoverEnterCallback = null;
+            _hoverExitCallback = null;
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void Init(Color activeColor, Color inactiveColor)
+        {
+            _active = false;
+            _activeColor = activeColor;
+            _inactiveColor = inactiveColor;
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void RegisterCallback(int index, System.Action<int> clickCallback,
+            System.Action<int> hoverEnterCallback, System.Action<int> hoverExitCallback)
+        {
+            _index = index;
+            _clickCallback = clickCallback;
+            _hoverEnterCallback = hoverEnterCallback;
+            _hoverExitCallback = hoverExitCallback;
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            if (_clickCallback != null)
+                _clickCallback(_index);
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void OnPointerEnter(PointerEventData pointerEventData)
+        {
+            if (_hoverEnterCallback != null)
+                _hoverEnterCallback(_index);
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------
+        public void OnPointerExit(PointerEventData pointerEventData)
+        {
+            if (_hoverExitCallback != null)
+                _hoverExitCallback(_index);
+        }
+    }
+}
